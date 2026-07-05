3ac3ebecc4659a650f7c7ebe3314601141b472da
ArifDev-coder <achadzainul67@gmail.com>
2026-07-05 21:05:27 +0700

    testing

--- ArduinoTrasmitter/src/main.cpp
+++ ArduinoTrasmitter/src/main.cpp
@@ -1,79 +1,28 @@
 #include <Arduino.h>
-#include <RF24.h>
 #include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
 
-// Pin analog pada joystick
-// From right to left joystick.
-const byte X1 = A1;
-const byte Y1 = A2;
-const byte X2 = A3;
-const byte Y2 = A5;
-
-// Struktur awal variable Controller
-struct ControlData
-{
-  byte throttle;
-  byte yaw;
-  byte pitch;
-  byte roll;
-};
-
-ControlData data;
-
-// Radio CE, CSN
 RF24 radio(10, 9);
+const byte address[6] = "00001";
 
-const byte pipe_addres[6] = "12301";
+uint32_t counter = 0;
 
-void resetData()
-{
-  data.throttle = 0;
-  data.yaw = 127;
-  data.pitch = 127;
-  data.roll = 127;
-}
-
-void setup()
-{
-  radio.begin();
-  radio.setAutoAck(false);
-  radio.setDataRate(RF24_250KBPS);
-  radio.openWritingPipe(pipe_addres);
-  radio.stopListening();
-
-  resetData();
-
+void setup() {
   Serial.begin(9600);
+  radio.begin();
+  radio.setAutoAck(false);          // matikan ack
+  radio.setChannel(100);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.setDataRate(RF24_250KBPS);
+  radio.openWritingPipe(address);
+  radio.stopListening();
 }
 
-// Remapping value mentah joystick
-int mapJoystickValue(int val, int lower, int middle, int upper, bool isReverse = false)
-{
-  val = constrain(val, lower, upper);
-
-  if (val < middle)
-  {
-    val = map(val, lower, middle, 0, 128);
-  }
-  else
-  {
-    val = map(val, middle, upper, 128, 255);
-  }
-
-  return {isReverse ? 255 - val : val};
-}
-
-void loop()
-{
-  data.throttle = mapJoystickValue(analogRead(X2), 0, 390, 1014, true);
-  data.yaw = mapJoystickValue(analogRead(Y2), 0, 408, 1007, true);
-  data.pitch = mapJoystickValue(analogRead(X1), 0, 386, 911, true);
-  data.roll = mapJoystickValue(analogRead(Y1), 0, 413, 991, true);
-
-  Serial.print(data.throttle); Serial.print(",");
-  Serial.print(data.yaw);      Serial.print(",");
-  Serial.print(data.pitch);    Serial.print(",");
-  Serial.println(data.roll);
-
-  radio.write(&data, sizeof(ControlData));
+void loop() {
+  radio.write(&counter, sizeof(counter));
+  Serial.print("Sent: ");
+  Serial.println(counter);
+  counter++;
+  delay(100); // biar gampang diamati, nanti bisa dipercepat
 }
--- DroneTest/src/main.cpp
+++ DroneTest/src/main.cpp
@@ -1,49 +1,43 @@
 #include <Arduino.h>
-#include <RF24.h>
 #include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
 
-// Radio CE, CSN
-RF24 radio(7, 10);
+RF24 radio(9, 10);
+const byte address[6] = "00001";
 
-// Pipe Address
-const byte pipe_address[6] = "12301";
+uint32_t lastCounter = 0;
+uint32_t received = 0;
+uint32_t expected = 0;
 
-struct DataControlPackage
-{
-  byte throttle;
-  byte yaw;
-  byte pitch;
-  byte roll;
-};
-
-DataControlPackage receivedData;
-
-void setup()
-{
+void setup() {
   Serial.begin(9600);
-
   radio.begin();
-  radio.setAutoAck(false);
+  radio.setAutoAck(false);          // matikan ack
+  radio.setChannel(100);
+  radio.setPALevel(RF24_PA_LOW);
   radio.setDataRate(RF24_250KBPS);
-  radio.openReadingPipe(0, pipe_address);
+  radio.openReadingPipe(0, address);
   radio.startListening();
 }
 
-void loop()
-{
-  if (radio.available())
-  {
-    radio.read(&receivedData, sizeof(DataControlPackage));
+void loop() {
+  if (radio.available()) {
+    uint32_t incoming;
+    radio.read(&incoming, sizeof(incoming));
 
-    Serial.print("Throttle: ");
-    Serial.print(receivedData.throttle);
-    Serial.print(" | Roll: ");
-    Serial.print(receivedData.roll);
-    Serial.print(" | Pitch: ");
-    Serial.print(receivedData.pitch);
-    Serial.print(" | Yaw: ");
-    Serial.println(receivedData.yaw);
+    received++;
+    expected = incoming - lastCounter - 1; // paket yang lompat/hilang
+
+    Serial.print("Diterima: ");
+    Serial.print(incoming);
+    if (expected > 0 && incoming > lastCounter) {
+      Serial.print("  <-- LOST ");
+      Serial.print(expected);
+      Serial.print(" paket!");
+    }
+    Serial.println();
+
+    lastCounter = incoming;
   }
-
-  delay(10);
 }
