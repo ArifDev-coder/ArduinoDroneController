5399d143cf9390755a1399b930c523925cf11124
ArifDev-coder <achadzainul67@gmail.com>
2026-07-26 20:37:08 +0700

    add setup for esp32 drone flight controller

---
+++ ESP-DRONE/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
+
+void setup()
+{
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    Serial.println("Testing...");
+    delay(1000);
+}
